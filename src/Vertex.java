768d337941252fcb265ffc53525cdbc758000fc7
KarpovichAlexandr220174 <kukareku_147@mail.ru>
2023-05-27 15:15:36 +0600

    Commit5

--- src/Vertex.java
+++ src/Vertex.java
@@ -4,8 +4,21 @@ public class Vertex<V> {
     private V data;
     private Map<Vertex<V>, Double> adjacentVertices;
 
+    public V getData() {
+        return data;
+    }
+
     public void addAdjacentVertex(Vertex<V> destination, double weight) {
         adjacentVertices.put(destination, weight);
+    }
+
+
+    public Map<Vertex<V>, Double> getAdjacentVertices() {
+        return adjacentVertices;
+    }
+
+    public String toString() {
+        return String.valueOf(data);
     }
 
 
